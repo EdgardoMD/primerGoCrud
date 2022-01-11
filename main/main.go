0fffa0c9678de398135e79b0373c0b75fabf175a
Edgardo Mora <edgardomora@hotmail.com>
2022-01-10 23:04:09 -0300

    refactor nombre modelo + agrega Index en main

--- main/main.go
+++ main/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"primerGoCrud/model"
 	"text/template"
 )
 
@@ -19,3 +21,29 @@ func dbConn() (db *sql.DB) {
 }
 
 var tmpl = template.Must(template.ParseGlob("form/*"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	selDB, error := db.Query("select * from Employee order by id desc")
+	if error != nil {
+		panic(error.Error())
+	}
+	emp := model.Empleado{}
+	res := []model.Empleado{}
+	for selDB.Next() {
+		var id int
+		var nombre, apellido, ciudad string
+		error = selDB.Scan(&id, &nombre, &apellido, &ciudad)
+		if error != nil {
+			panic(error.Error())
+		}
+
+		emp.Id = id
+		emp.Nombre = nombre
+		emp.Apellido = apellido
+		emp.Ciudad = ciudad
+		res = append(res, emp)
+	}
+	tmpl.ExecuteTemplate(w, "Index", res)
+	defer db.Close()
+}
