14443e90b2a9fcd5cdb657c2a6d488626be5df6e
Edgardo Mora <edgardomora@hotmail.com>
2022-01-12 13:11:53 -0300

    completa CRUD

--- main/main.go
+++ main/main.go
@@ -96,11 +96,70 @@ func Insertar(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", 301)
 }
 
+func Editar(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nId := r.URL.Query().Get("id")
+	selDB, err := db.Query("SELECT * FROM Empleado WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := model.Empleado{}
+	for selDB.Next() {
+		var id int
+		var nombre, apellido, ciudad string
+		err = selDB.Scan(&id, &nombre, &apellido, &ciudad)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.Nombre = nombre
+		emp.Apellido = apellido
+		emp.Ciudad = ciudad
+	}
+	tmpl.ExecuteTemplate(w, "Editar", emp)
+	defer db.Close()
+}
+
+func Actualizar(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		nombre := r.FormValue("nombre")
+		apellido := r.FormValue("apellido")
+		ciudad := r.FormValue("ciudad")
+		id := r.FormValue("uid")
+		insertarForm, error := db.Prepare("UPDATE Empleado SET nombre=?, apellido=?, ciudad=? WHERE id = ?")
+		if error != nil {
+			panic(error.Error())
+		}
+		insertarForm.Exec(nombre, apellido, ciudad, id)
+		log.Printf("Se ha actualizado al Empleado: " + nombre + ", " + apellido + "que vive en " + ciudad)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+
+}
+
+func Eliminar(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	emp := r.URL.Query().Get("id")
+	eliminar, err := db.Prepare("delete from Empleado where id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	eliminar.Exec(emp)
+	log.Println("Eliminado")
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
 func main() {
-	log.Println("Servidor corriendo en puerto: http://localhost:8090")
+	log.Println("Servidor corriendo en puerto: http://localhost:8091")
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/mostrar", Mostrar)
 	http.HandleFunc("/nuevo", Nuevo)
+	http.HandleFunc("/editar", Editar)
+	http.HandleFunc("/actualizar", Actualizar)
 	http.HandleFunc("/insertar", Insertar)
-	http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/eliminar", Eliminar)
+	http.ListenAndServe(":8091", nil)
 }
